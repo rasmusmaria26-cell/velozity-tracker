883a510e29d2b56cb3d121c03cd0229f3a93032e
rasmusmaria26-cell <rasmusmaria26@gmail.com>
2026-03-23 13:47:58 +0530

    fix: add filters state to useMemo dependency array for reactive sorting

--- src/views/KanbanView.tsx
+++ src/views/KanbanView.tsx
@@ -14,7 +14,11 @@ const COLUMNS = [
 const KanbanView = () => {
     const tasks = useTaskStore((s) => s.tasks)
     const applyFilters = useFilterStore((s) => s.applyFilters)
-    const filteredTasks = useMemo(() => applyFilters(tasks), [tasks, applyFilters])
+    const filters = useFilterStore((s) => s.filters)
+    const filteredTasks = useMemo(
+        () => applyFilters(tasks),
+        [tasks, filters, applyFilters]
+    )
 
     return (
         <div className="flex gap-4 h-full overflow-x-auto px-4 pb-4 pt-4">
